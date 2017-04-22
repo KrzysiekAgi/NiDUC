9a6cc164b9c6a6796e7054f8e40b9ba3b8b3036b
Filip Olszewski <filipolszew@o2.pl>
2017-04-22 18:46:06 +0200

    kodowanie 2z5 v1

--- koduj2z5.m
+++ koduj2z5.m
@@ -2,6 +2,18 @@ function [codedData] = koduj2z5( data )
 %KODUJ2Z5 Summary of this function goes here
 %   Detailed explanation goes here
 
+codedData = [];
 
+for i = 1:size(data,1)
+    v = [];
+    for j = 1:size(data,2)
+        if data(i,j) == 0
+            v = [v 1 1 0 0 0];
+        else
+            v = [v 1 0 1 0 0];
+        end
+    end
+    codedData = [codedData;v];
+end 
 end
 
