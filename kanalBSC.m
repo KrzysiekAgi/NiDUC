64813eab0812c6f945e7b55b7c1cae725e52261c
KrzysiekAgi <225970@student.pwr.edu.pl>
2017-04-22 14:53:51 +0200

    Update kanalBSC.m
    zapomniałem średnika

--- kanalBSC.m
+++ kanalBSC.m
@@ -1,9 +1,9 @@
 function [ corruptedData ] = kanalBSC( data, p )
 %KANALBSC Summary of this function goes here
 %   Detailed explanation goes here
-%   data to macierz danych, p to prawdopodobieñstwo zak³ócenia
-%   zak³adam, ¿e p<=0.5, inaczej nale¿y zrobiæ zamianê - odebrane 0
-%   interpretujemy jako 1, a odebran¹ 1 jako 0. Wtedy p = 1 - p
-    corruptedData = bsc(data, p)
+%   data to macierz danych, p to prawdopodobieÃ±stwo zakÂ³Ã³cenia
+%   zakÂ³adam, Â¿e p<=0.5, inaczej naleÂ¿y zrobiÃ¦ zamianÃª - odebrane 0
+%   interpretujemy jako 1, a odebranÂ¹ 1 jako 0. Wtedy p = 1 - p
+    corruptedData = bsc(data, p);
 end
 
