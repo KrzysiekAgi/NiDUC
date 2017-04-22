10935f50e8c063ab58448feca5a5074d95757ef0
kubaper <kubapere@gmail.com>
2017-04-22 11:39:35 +0200

    Add files via upload
    gotowa(?) funkcja

--- generatePackets.m
+++ generatePackets.m
@@ -1,7 +1,7 @@
-function [dane] = generatePackets(iloscPakietow, rozmiarPakietu)
+function dane = generatePackets(iloscPakietow, rozmiarPakietu)
 %GENERATE Summary of this function goes here
 %   Detailed explanation goes here
-
+dane=randi([0 1], iloscPakietow,rozmiarPakietu);
 
 end
 
