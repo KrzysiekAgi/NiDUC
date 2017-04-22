3872776bf5dca7b0cd941318b71b6a670ddcd99c
KrzysiekAgi <225970@student.pwr.edu.pl>
2017-04-22 14:50:54 +0200

    kanał BSC z wykorzystaniem f-cji bsc()

--- kanalBSC.m
+++ kanalBSC.m
@@ -1,7 +1,9 @@
-function [ output_args ] = kanalBSC( input_args )
+function [ corruptedData ] = kanalBSC( data, p )
 %KANALBSC Summary of this function goes here
 %   Detailed explanation goes here
-
-
+%   data to macierz danych, p to prawdopodobieñstwo zak³ócenia
+%   zak³adam, ¿e p<=0.5, inaczej nale¿y zrobiæ zamianê - odebrane 0
+%   interpretujemy jako 1, a odebran¹ 1 jako 0. Wtedy p = 1 - p
+    corruptedData = bsc(data, p)
 end
 
