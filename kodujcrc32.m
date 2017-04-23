4d65055debe46edb7445b31b6e02e7368ee57600
KrzysiekAgi <225970@student.pwr.edu.pl>
2017-04-23 19:21:47 +0200

    pierwsze próby do crc32
    Jeśli ktoś wpadnie na lepszy pomysł, to zmieniać jak najszybciej :P 
    Reference: https://www.mathworks.com/help/comm/ref/comm.crcgenerator-class.html

--- kodujcrc32.m
+++ kodujcrc32.m
@@ -1,7 +1,13 @@
 function [ codedData ] = kodujcrc32( data )
 %KODUJCRC32 Summary of this function goes here
-%   Detailed explanation goes here
-
+%   w gen tworzê generator CRC z wielomianem takim jak w nawiasach
+%   kwadratowych (znalaz³em ¿e taki jest dla 32b), tworz¹cy dwie
+%   "subframes". Tê iloœæ zmieniamy ostatnim argumentem. F-cja step tworzy
+%   zakodowany ci¹g i dodaje go do wektora data. Do detekcji mo¿na u¿yæ
+%   obiektu comm.CRCDetector
+%   Jeœli ktoœ ma lepszy pomys³, to zmieniajcie ten kod :P 
+gen = comm.CRCGenerator([1 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 1],'ChecksumsPerFrame',2);
+codedData = step(gen, data);
 
 end
 
