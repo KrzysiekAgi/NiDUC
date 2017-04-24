137ffbad605300a5f5856cbbdfcae2a5c04daf5f
Kajetan <kajetan123@gmail.com>
2017-04-24 13:40:04 +0200

    Dekodowanie CRC32
    --

--- dekodujcrc32.m
+++ dekodujcrc32.m
@@ -1,7 +1,20 @@
 function [IsValid, uncodedData] = dekodujcrc32( data )
 %DEKODUJCRC32 Summary of this function goes here
 %   Detailed explanation goes here
-
+[m n] = size(data);
+poly = [1 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 1];
+uncodedData = data(1,1:n-32);
+manipulatedData = [uncodedData 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+for i=1:n-32
+    if manipulatedData(i) == 1
+        manipulatedData(1,i:i+32) = bitxor(manipulatedData(1,i:i+32), poly);
+    end
+end
+if manipulatedData(1,n-31:n) == data(1,n-31:n)
+    IsValid = 1;
+else
+    IsValid = 0;
+end
 
 end
 
