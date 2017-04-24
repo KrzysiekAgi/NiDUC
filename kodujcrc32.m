6bb36cef08e8c4fef775136e6425414ad398b203
Kajetan <kajetan123@gmail.com>
2017-04-24 12:27:37 +0200

    Poprawione kodowanie CRC32
    --

--- kodujcrc32.m
+++ kodujcrc32.m
@@ -6,8 +6,21 @@ function [ codedData ] = kodujcrc32( data )
 %   zakodowany ci¹g i dodaje go do wektora data. Do detekcji mo¿na u¿yæ
 %   obiektu comm.CRCDetector
 %   Jeœli ktoœ ma lepszy pomys³, to zmieniajcie ten kod :P 
-gen = comm.CRCGenerator([1 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 1],'ChecksumsPerFrame',2);
-codedData = step(gen, data);
+%   No to poprawi³em - tamto nie chcia³o dzia³aæ xd
+[m n] = size(data);
+poly = [1 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 1];
+codedData = [];
+for i=1:m
+    codedData = [codedData data(i,:)];
+    manipulatedData = [data(i,:) 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+    for j=1:n
+        if manipulatedData(j)
+            manipulatedData(1,j:j+32) = bitxor(manipulatedData(1,j:j+32),poly);
+        end
+    end
+    codedData = [codedData manipulatedData(1,n+1:n+32)];
+end
 
+codedData = vec2mat(codedData,n+32);
 end
 
