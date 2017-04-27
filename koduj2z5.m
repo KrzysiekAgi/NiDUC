2d76361f4ef019e7f9f5d4b18677b763a3289db0
Filip Olszewski <filipolszew@o2.pl>
2017-04-27 19:11:02 +0200

    kodowanie2z5 optymalizacja/refactor

--- koduj2z5.m
+++ koduj2z5.m
@@ -2,18 +2,19 @@ function [codedData] = koduj2z5( data )
 %KODUJ2Z5 Summary of this function goes here
 %   Detailed explanation goes here
 
-codedData = [];
 
-for i = 1:size(data,1)
-    v = [];
-    for j = 1:size(data,2)
+[m, n] = size(data);
+codedData = zeros(m, n);
+
+for i = 1:m
+    for j = 1:n
+        x = 5*(j-1) + 1;
         if data(i,j) == 0
-            v = [v 1 1 0 0 0];
+            codedData(i, x:x+4) = [1, 1 , 0, 0, 0];
         else
-            v = [v 1 0 1 0 0];
+            codedData(i, x:x+4) = [1, 0 , 1, 0, 0];
         end
     end
-    codedData = [codedData;v];
 end 
 end
 
