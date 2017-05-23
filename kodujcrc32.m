b177c4a1bb6ba8193b2846d95bc8e763352707b1
KrzysiekAgi <225970@student.pwr.edu.pl>
2017-05-23 16:24:40 +0200

    Update kodujcrc32.m

--- kodujcrc32.m
+++ kodujcrc32.m
@@ -1,12 +1,6 @@
 function [ codedData ] = kodujcrc32( data )
 %KODUJCRC32 Summary of this function goes here
-%   w gen tworzê generator CRC z wielomianem takim jak w nawiasach
-%   kwadratowych (znalaz³em ¿e taki jest dla 32b), tworz¹cy dwie
-%   "subframes". Tê iloœæ zmieniamy ostatnim argumentem. F-cja step tworzy
-%   zakodowany ci¹g i dodaje go do wektora data. Do detekcji mo¿na u¿yæ
-%   obiektu comm.CRCDetector
-%   Jeœli ktoœ ma lepszy pomys³, to zmieniajcie ten kod :P 
-%   No to poprawi³em - tamto nie chcia³o dzia³aæ xd
+
 [m n] = size(data);
 poly = [1 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 1];
 codedData = [];
