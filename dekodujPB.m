e4780e3038008a6f487d7faef51cbb4e983f51c1
Kajetan <kajetan123@gmail.com>
2017-05-23 17:52:09 +0200

    literówka/wypisywanie zmiennych
    --

--- dekodujPB.m
+++ dekodujPB.m
@@ -3,15 +3,15 @@ function [IsReceived, uncodedData] = dekodujPB( receivedPacket )
 %   Detailed explanation goes here
 %   dekodowanie/kontrola b³êdów za pomoc¹ bitu parzystoœci
 
-parityBit=recievedPacket(end)
-recievedData=recievedPacket(1:end-1);
-parityTest = mod(sum(recievedData),2)
+parityBit=receivedPacket(end);
+receivedData=receivedPacket(1:end-1);
+parityTest = mod(sum(receivedData),2);
 if parityTest==parityBit
-    IsRecieved = 1
-  else
-    IsRecieved = 0
-  endif
- uncodedData=recievedData
+    IsReceived = 1;
+else
+    IsReceived = 0;
+end
+uncodedData=receivedData;
 
 end
 
--- kodujPB.m
+++ kodujPB.m
@@ -4,6 +4,6 @@ function [codedData] = kodujPB( data )
 %   kodowanie/kontrola b³êdów za pomoc¹ bitu parzystoœci
 
 %pisane w Octavie, jeśli Matlab wywali syntax error, to pierwsze co sprawdźcie zmianę mod(sum(data),2) na sum(data) mod 2
-  codedData = [data, mod(sum(data),2)]
+  codedData = [data, mod(sum(data),2)];
 end
 
