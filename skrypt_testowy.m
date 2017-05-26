d59b5d892ff3b0de6b8ec84ae8f25d0a64c6e68b
Filip Olszewski <filipolszew@o2.pl>
2017-05-26 12:20:17 +0200

    extended simulation script + some ready, useful data generated

--- skrypt_testowy.m
+++ skrypt_testowy.m
@@ -3,16 +3,22 @@ AverageOT = 0;
 AverageRPC = 0;
 NumberOfSimulations = 1000;
 
+startErrorRate = 0.0000;
+
+for j = 1:50
+
+    startErrorRate = startErrorRate + 0.0002;
+    
 for i = 1:NumberOfSimulations
     
     x = Simulation();
     x.setModelVer('BSC') % BSC, BEC, CEC
     x.setErrorControlVer('2z5') % CRC32, 2z5, PB
-    x.setProtocolVer('GBN') % SAW , GBN
-    x.setPacketSize(16) % 8/16/64/256/512
-    x.setPacketsCount(100) % 100
+    x.setProtocolVer('SAW') % SAW , GBN
+    x.setPacketSize(8) % 8/16/64/256/512
+    x.setPacketsCount(10) % 10
     x.setBitTransRate(1000) % raczej nie zmieniac
-    x.setErrorRate(0.001) % 0 - 0.5
+    x.setErrorRate(startErrorRate) % 0 - 0.5
     
     [BER, OT, RPC] = x.simulate();
     
@@ -29,3 +35,5 @@ fileID = fopen('data_skrypt.txt','a');
 format = '%s;%s;%s;%d;%d;%d;%f;%f;%f;%f\n';
 fprintf(fileID, format, x.ModelVer, x.ErrorControlVer, x.ProtocolVer, x.PacketSize, x.PacketsCount, x.BitTransmissionRate, x.ErrorRate, AverageBER, AverageOT, AverageRPC);
 fclose(fileID);
+AverageBER
+end
