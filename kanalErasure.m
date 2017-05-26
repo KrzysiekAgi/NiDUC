5e6367ba0c0ac436e88112fb18e019f5338dd00f
KrzysiekAgi <225970@student.pwr.edu.pl>
2017-05-26 11:54:47 +0200

    komentarz

--- kanalErasure.m
+++ kanalErasure.m
@@ -1,5 +1,5 @@
 function [corruptedData] = kanalErasure (data, p)
-[m,n]=size(data);
+[m,n]=size(data); % tak de facto to jest to wektor (powstanie macierz o wymiarach coś x 1 )
 j=1;
    for i=1:(n*m)
      if rand(1,1)>p
